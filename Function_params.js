88d7745994efc5c7a7a0c61c57faee08723d363e
Preetha2018 <135413266+Preetha2018@users.noreply.github.com>
2023-12-01 20:31:01 +0530

    Create Function_params.js

---
+++ Function_params.js
@@ -0,0 +1,8 @@
+// Function to add two numbers
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+
+// Function call and output
+var result = addNumbers(5, 3);
+console.log("Sum:", result);
