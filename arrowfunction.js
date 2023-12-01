e9b36e058995e90540c617d1d4ee336885670948
Preetha2018 <135413266+Preetha2018@users.noreply.github.com>
2023-12-01 20:31:53 +0530

    Create arrowfunction.js

---
+++ arrowfunction.js
@@ -0,0 +1,8 @@
+// Arrow function to greet a user
+const greetUser = (name) => {
+    return `Hello, ${name}!`;
+};
+
+// Function call and output
+const userName = "Alice";
+console.log(greetUser(userName));
