29e02465e0acfe7db96465e4c4fc83a0be7edc3f
Preetha2018 <135413266+Preetha2018@users.noreply.github.com>
2023-12-01 20:42:49 +0530

    Create operatorexample.js

---
+++ operatorexample.js
@@ -0,0 +1,54 @@
+// Arithmetic operators
+let num1 = 10;
+let num2 = 5;
+
+let addition = num1 + num2;
+console.log("Addition:", addition);
+
+let subtraction = num1 - num2;
+console.log("Subtraction:", subtraction);
+
+let multiplication = num1 * num2;
+console.log("Multiplication:", multiplication);
+
+let division = num1 / num2;
+console.log("Division:", division);
+
+let remainder = num1 % num2;
+console.log("Remainder:", remainder);
+
+// Comparison operators
+let isEqual = num1 === num2;
+console.log("Is Equal:", isEqual);
+
+let isNotEqual = num1 !== num2;
+console.log("Is Not Equal:", isNotEqual);
+
+let isGreaterThan = num1 > num2;
+console.log("Is Greater Than:", isGreaterThan);
+
+let isLessThan = num1 < num2;
+console.log("Is Less Than:", isLessThan);
+
+let isGreaterOrEqual = num1 >= num2;
+console.log("Is Greater or Equal:", isGreaterOrEqual);
+
+let isLessOrEqual = num1 <= num2;
+console.log("Is Less or Equal:", isLessOrEqual);
+
+// Logical operators
+let logicalAnd = (num1 > 0) && (num2 > 0);
+console.log("Logical AND:", logicalAnd);
+
+let logicalOr = (num1 > 0) || (num2 > 0);
+console.log("Logical OR:", logicalOr);
+
+let logicalNot = !(num1 > 0);
+console.log("Logical NOT:", logicalNot);
+
+// Increment and Decrement operators
+let incrementResult = num1++;
+console.log("Increment Result:", incrementResult, "Updated num1:", num1);
+
+let decrementResult = num2--;
+console.log("Decrement Result:", decrementResult, "Updated num2:", num2);
